3c6dd77f15c3a91ba45e8ace316db63559082215
JaqEst <jade2ggle@gmail.com>
2024-04-17 21:28:02 +0800

    style(应用配置): 调整应用配置参数顺序

--- AuthMicroservices/config.py
+++ AuthMicroservices/config.py
@@ -1,20 +1,31 @@
 class Config:
     DEBUG = False
     TESTING = False
+
+
+class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/iot'
     SQLALCHEMY_ECHO = False
 
+    CORS_RESOURCES = {r'/api/*': {'origins': '*'}}
+
 
 class DevelopmentConfig(Config):
-    DEBUG = True    # 启用调试模式(目前似乎没效果)
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/iot'
     SQLALCHEMY_ECHO = True
 
 
 class TestingConfig(Config):
     TESTING = True
 
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/iot'
+    SQLALCHEMY_ECHO = True
+
 
 config = {
+    'production': ProductionConfig,
     'development': DevelopmentConfig,
-    'testing': TestingConfig
+    'testing': TestingConfig,
 }
